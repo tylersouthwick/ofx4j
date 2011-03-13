8eeea3c4cb2d56a296cb860d7a830bfc89732842
Tyler Southwick <tyler@northfuse.net>
2011-03-13 16:40:59 -0600

    added write method to write an OFX object

--- src/main/scala/net/northfuse/ofx/OfxWriter.scala
+++ src/main/scala/net/northfuse/ofx/OfxWriter.scala
@@ -1,6 +1,11 @@
 package net.northfuse.ofx
 
+import net.northfuse.ofx.domain._
+import java.io.OutputStream
+
 object OfxWriter {
-	val context = javax.xml.bind.JAXBContext.newInstance(classOf[net.northfuse.ofx.domain.ObjectFactory])
+	val context = javax.xml.bind.JAXBContext.newInstance(classOf[ObjectFactory])
 	val marshaller = context.createMarshaller
+
+	def write(ofx : OFX, out: OutputStream) = marshaller.marshal(ofx, out)
 }
