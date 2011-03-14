83462d066027d33acd903469d0769c589298f2fe
Tyler Southwick <tyler@northfuse.net>
2011-03-13 19:13:38 -0600

    added unmarshal support

--- src/main/scala/net/northfuse/ofx/OfxWriter.scala
+++ src/main/scala/net/northfuse/ofx/OfxWriter.scala
@@ -1,11 +1,14 @@
 package net.northfuse.ofx
 
 import net.northfuse.ofx.domain._
-import java.io.OutputStream
+import java.io._
 
 object OfxWriter {
 	val context = javax.xml.bind.JAXBContext.newInstance(classOf[ObjectFactory])
 	val marshaller = context.createMarshaller
+	val unmarshaller = context.createUnmarshaller
 
 	def write(ofx : OFX, out: OutputStream) = marshaller.marshal(ofx, out)
+
+	def read(in : InputStream) = unmarshaller.unmarshal(in).asInstanceOf[OFX]
 }
